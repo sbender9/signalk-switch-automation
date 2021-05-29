6066a2b9806f32aec9df565d9dfe6779b65a45e0
Christian Wegerhoff <wegerhoff@mediaworx.com>
2021-05-29 11:28:39 +0200

    feature: define cooldown time (debounce interval) for each relay feature: dashboard status messages

--- index.js
+++ index.js
@@ -34,32 +34,33 @@ module.exports = function(app) {
   }
 
   plugin.start = function(theOptions) {
-
+    app.setPluginStatus('Started')
     if ( !app.putSelfPath ) {
       statusMessage = "Please upgrade your node server, your version does not support put requests"
       return
     }
-    
+
     options = theOptions;
-    
+
     options.relays.forEach((relay) => {
       if ( _.isUndefined(relay.enabled) || relay.enabled ) {
         var paths = (relay.conditions || []).map(c => c.path)
-        app.debug(`paths: ${relay.relayPath}: ${JSON.stringify(paths)}`)
+        app.debug(`paths: ${relay.relayPath}: ${JSON.stringify(paths)}, cooldown: ${relay.cooldown}`)
         unsubscribes.push(
           Bacon.combineWith(
             evaluator.bind(this, relay.conditions),
             paths.map(app.streambundle.getSelfStream, app.streambundle)
           )
             .changes()
-            .debounceImmediate(20000)
+            .debounceImmediate(_.isUndefined(relay.cooldown) ? 3000 : relay.cooldown)
             .onValue(state => {
               if ( !_.isUndefined(state) ) {
                 var current = app.getSelfPath(`${relay.relayPath}.value`)
                 if ( !_.isUndefined(current) && current != state ) {
                   app.debug(`sending new state ${state} for relay ${relay.relayPath}`)
+                  app.setPluginStatus(`sending state ${state} for ${relay.relayPath}`)
                   app.putSelfPath(relay.relayPath, state)
-                  
+
                   /*
                 app.emit('venusSetValue',
                 {
@@ -95,6 +96,7 @@ module.exports = function(app) {
         testValue = Number(cond.value)
         if ( testValue == NaN ) {
           console.log(`could not convert the condition value to a number ${cond.value}`)
+          app.setPluginStatus(`Invalid condition value ${cond.value}`)
           hadErrors = true
           return
         }
@@ -102,7 +104,7 @@ module.exports = function(app) {
 
       var test = cond.test
       if ( test === '==' ) {
-        testResult = value == testValue 
+        testResult = value == testValue
       } else if ( test === '!=' ) {
         testResult = value != testValue
       } else if ( test === '<' ) {
@@ -110,26 +112,27 @@ module.exports = function(app) {
       } else if ( test === '>' ) {
         testResult = value > testValue
       }
-      
+
       app.debug(`${index != 0 ? cond.operator : ''} ${value} ${cond.test} ${testValue} = ${testResult}`)
 
       state = index == 0 ? testResult : (cond.operator == 'And' ? state && testResult : state || testResult)
     });
     return !_.isUndefined(state) && !hadErrors ? (state ? 1 : 0) : undefined
   }
-  
+
   plugin.stop = function() {
     app.debug("stopping...")
     unsubscribes.forEach(f => f());
     unsubscribes = [];
+    app.setPluginStatus('Stopped')
   };
-  
+
   function stopSubscription()
   {
     onStop.forEach(f => f());
     onStop = []
   }
-  
+
   plugin.schema = function() {
     var paths = JSON.parse(JSON.stringify(app.streambundle.getAvailablePaths())).sort()
     var res = {
@@ -150,6 +153,12 @@ module.exports = function(app) {
                 type: 'string',
                 title: 'Relay Path',
                 enum: paths
+              },
+              cooldown: {
+                type: 'integer',
+                title: 'Cooldown Time',
+                default: 3000,
+                description: "Cooldown time before relay will process next message (in milliseconds)"
               },
               conditions: {
                 type: "array",
@@ -192,6 +201,6 @@ module.exports = function(app) {
     }
     return res
   }
-  
+
   return plugin;
 }
